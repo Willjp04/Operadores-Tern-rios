68f649db247fc208ebf85d93f53adc0e1730c697
Willian Sostenes <sostenes655@gmail.com>
2020-09-08 00:52:34 -0300

    Exemplo operadores ternários

---
+++ Operadores Ternários/src/operadores/ternários/OperadoresTernários.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadores.ternários;
+
+/**
+ *
+ * @author PORTOMASTER
+ */
+public class OperadoresTernários {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int n1,n2,r;
+       n1 = 16;
+       n2 = 15;
+       r = n1>n2 ? 0:1;
+        System.out.println(r);
+               
+    }
+    
+}
